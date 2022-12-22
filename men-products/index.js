98877bc6b0af5bc000f86051e3399dd83614e46a
alishabab <alishabab62@gmail.com>
2022-12-22 19:30:14 +0530

    Change

--- men-products/index.js
+++ men-products/index.js
@@ -15,109 +15,100 @@ const studio = document.querySelector(".studio");
 const profile = document.querySelector(".profile");
 const profileMouseOut = document.querySelector(".header_profile_submenu_div");
 
-function openSubmenuMen(){
-    // console.log("vineet");
-    men.style.display= "flex";
+function openSubmenuMen() {
+  // console.log("vineet");
+  men.style.display = "flex";
 }
-function closeSubmenuMen(){
-    men.style.display="none";
+function closeSubmenuMen() {
+  men.style.display = "none";
 }
 function openSubmenuWomen() {
-    console.log(women)
-    men.style.display= "flex";
+  console.log(women);
+  men.style.display = "flex";
 }
 function closeSubmenuWomen() {
-    console.log(women)
-    men.style.display= "none";
+  console.log(women);
+  men.style.display = "none";
 }
-function openSubmenuKids(){
-    men.style.display= "flex";
+function openSubmenuKids() {
+  men.style.display = "flex";
 }
-function closeSubmenuKids(){
-    men.style.display= "none";
+function closeSubmenuKids() {
+  men.style.display = "none";
 }
-function openSubmenuHome(){
-    home.style.display= "flex";
+function openSubmenuHome() {
+  home.style.display = "flex";
 }
-function closeSubmenuHome(){
-    home.style.display= "none";
+function closeSubmenuHome() {
+  home.style.display = "none";
 }
-function openSubmenuBeauty(){
-    beauty.style.display= "flex";
+function openSubmenuBeauty() {
+  beauty.style.display = "flex";
 }
-function closeSubmenuBeauty(){
-    beauty.style.display= "none";
+function closeSubmenuBeauty() {
+  beauty.style.display = "none";
 }
-function openSubmenuStudio(){
-    studio.style.display= "flex";
+function openSubmenuStudio() {
+  studio.style.display = "flex";
 }
-function closeSubmenuStudio(){
-    studio.style.display= "none";
+function closeSubmenuStudio() {
+  studio.style.display = "none";
 }
-function openSubmenuProfile(){
-    profile.style.display= "block";
+function openSubmenuProfile() {
+  profile.style.display = "block";
 }
-function closeSubmenuProfile(){
-    profile.style.display= "none";
+function closeSubmenuProfile() {
+  profile.style.display = "none";
 }
-header_men.addEventListener('mouseover', openSubmenuMen);
-header_men.addEventListener('mouseout', closeSubmenuMen);
-header_women.addEventListener('mouseover', openSubmenuWomen);
-header_women.addEventListener('mouseout', closeSubmenuWomen);
-header_kids.addEventListener('mouseover', openSubmenuKids);
-header_kids.addEventListener('mouseout', closeSubmenuKids);
-header_home.addEventListener('mouseover', openSubmenuHome);
-header_home.addEventListener('mouseout', closeSubmenuHome);
-header_beauty.addEventListener('mouseover', openSubmenuBeauty);
-header_beauty.addEventListener('mouseout', closeSubmenuBeauty);
-header_studio.addEventListener('mouseover', openSubmenuStudio);
-header_studio.addEventListener('mouseout', closeSubmenuStudio);
-header_profile.addEventListener('mouseover', openSubmenuProfile);
-profileMouseOut.addEventListener('mouseout', closeSubmenuProfile);
+header_men.addEventListener("mouseover", openSubmenuMen);
+header_men.addEventListener("mouseout", closeSubmenuMen);
+header_women.addEventListener("mouseover", openSubmenuWomen);
+header_women.addEventListener("mouseout", closeSubmenuWomen);
+header_kids.addEventListener("mouseover", openSubmenuKids);
+header_kids.addEventListener("mouseout", closeSubmenuKids);
+header_home.addEventListener("mouseover", openSubmenuHome);
+header_home.addEventListener("mouseout", closeSubmenuHome);
+header_beauty.addEventListener("mouseover", openSubmenuBeauty);
+header_beauty.addEventListener("mouseout", closeSubmenuBeauty);
+header_studio.addEventListener("mouseover", openSubmenuStudio);
+header_studio.addEventListener("mouseout", closeSubmenuStudio);
+header_profile.addEventListener("mouseover", openSubmenuProfile);
+profileMouseOut.addEventListener("mouseout", closeSubmenuProfile);
 
 // ===========================For Tab and Mobile view=================
 
 const burgerIcon = document.querySelector("#burgerIcon");
-const sidebar = document.querySelector(".sidebarMenu")
+const sidebar = document.querySelector(".sidebarMenu");
 
-function openSideBar(){
-    // console.log("vineet");
-    // sidebar.style.display="block"
-    if(sidebar.style.display !=="block"){
-        sidebar.style.display = "block"
-    }else{
-        sidebar.style.display = "none";
-    }
+function openSideBar() {
+  // console.log("vineet");
+  // sidebar.style.display="block"
+  if (sidebar.style.display !== "block") {
+    sidebar.style.display = "block";
+  } else {
+    sidebar.style.display = "none";
+  }
 }
 
+burgerIcon.addEventListener("click", openSideBar);
 
-burgerIcon.addEventListener('click', openSideBar);
-
-
-
-
-
-
-
-
-
-
-
-
-let addProduct=document.getElementById('add-product');
-let productDiv="";
-fetch('https://fakestoreapi.com/products').then((data)=>{
+let addProduct = document.getElementById("add-product");
+let productDiv = "";
+fetch("https://fakestoreapi.com/products")
+  .then((data) => {
     return data.json();
-}).then((data2)=>{
-    for(let item of data2){
-        let product=document.createElement('div');
-        product.classList="product"
-        product.innerHTML=`<img src="${item.image}" alt="">
+  })
+  .then((data2) => {
+    for (let item of data2) {
+      let product = document.createElement("div");
+      product.classList = "product";
+      product.innerHTML = `<img src="${item.image}" alt="">
         <div class="item-name">${item.title}</div>
         <div class="description">${item.description}</div>
-        <div class="price">Rs.${item.price}</div>`
-        addProduct.appendChild(product)
-        console.log(item)
+        <div class="price">Rs.${item.price}<span id="discount-price">${
+        item.price * 2
+      }</span><span id="discount-percentage">(50% OFF)</span></span></div>`;
+      addProduct.appendChild(product);
+      console.log(item);
     }
-})
-
+  });
