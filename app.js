85b278e4dd9641d135be6dd59406154f30d58f40
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2022-12-24 18:02:23 +0530

    complete_Responsive_Header_&Add_Add_new_Functionality_timer

--- app.js
+++ app.js
@@ -104,4 +104,20 @@ function openSideBar(){
 
 burgerIcon.addEventListener('click', openSideBar);
 
+// ===================== Sale End Timer=================================
+
+const startingTime = 60;
+let time = startingTime *60;
+
+const timer = document.querySelector('.timer');
+
+setInterval(timeupdate, 1000);
+
+function timeupdate() {
+    const min = Math.floor(time/60);
+    let sec = time % 60;
+
+    timer.innerHTML = `${min}: ${sec}`;
+    time--;
+}
 // =================================vineet's js code end======================
