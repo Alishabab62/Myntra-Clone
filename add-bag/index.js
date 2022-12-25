ec53e9424371369c3f944105d5f7590a324a90ba
kspl2022 <kspltech2022@gmail.com>
2022-12-25 19:17:45 +0530

    Remove Item Problem Solved

--- add-bag/index.js
+++ add-bag/index.js
@@ -1,22 +1,19 @@
-
-const logo = document.querySelector(".logo")
-
-logo.addEventListener('click',(()=>{
-    location = '../index.html';
-   
-}))
+const logo = document.querySelector(".logo");
+logo.addEventListener("click", () => {
+  location = "../index.html";
+});
 
 // ================================================
 
-const mainBag=document.querySelector('.product-bag');
-const totalAmount=document.querySelector('#total-amount');
-const totalAmountDiscount=document.querySelector('#total-amount-discount');
-const totalItem=document.querySelector('#total-item');
-window.addEventListener('load',()=>{
-    let a=localStorage.getItem("productArray");
-    let productObj=(JSON.parse(a));
-    productObj.forEach((product)=>{
-        mainBag.innerHTML+=` <div class="product">
+const mainBag = document.querySelector(".product-bag");
+const totalAmount = document.querySelector("#total-amount");
+const totalAmountDiscount = document.querySelector("#total-amount-discount");
+const totalItem = document.querySelector("#total-item");
+window.addEventListener("load", () => {
+  let a = localStorage.getItem("productArray");
+  let productObj = JSON.parse(a);
+  productObj.forEach((product) => {
+    mainBag.innerHTML += ` <div class="product">
         <div class="image"><img src="${product.itemLink}" alt=""></div>
         <div class="description">
             <div class="item-name">${product.itemName}</div>
@@ -39,51 +36,49 @@ window.addEventListener('load',()=>{
             </div>
            <div class="remove-box"><div class="price"><span>Rs.</span><span>${product.price}</span></div><button class="remove-btn">Remove</button></div>
         </div>
-    </div>`
-    })
-    let price=0;
-    productObj.forEach((product)=>{
-        let p=Number(product.price);
-        price += p;
-    });
-    let total=productObj.length;
-    totalAmount.innerText=price;               //totalAmount
-    totalAmountDiscount.innerText=price;      //totalAmountDiscount
-    totalItem.innerText=total;                 //totalItem
-    localStorage.setItem("totalMrp",price);
-    localStorage.setItem("totalProduct",total);   
-    
+    </div>`;
+  });
+  let price = 0;
+  productObj.forEach((product) => {
+    let p = Number(product.price);
+    price += p;
+    console.log(p);
+  });
+  let total = productObj.length;
+  totalAmount.innerText = price; //totalAmount
+  totalAmountDiscount.innerText = price; //totalAmountDiscount
+  totalItem.innerText = total; //totalItem
+  localStorage.setItem("totalMrp", price);
+  localStorage.setItem("totalProduct", total);
 });
 
 // function to remove item
 
-mainBag.addEventListener('click',(e)=>{
-    if(e.target.classList.contains('remove-btn')){
-        e.target.parentNode.parentNode.parentNode.remove();
-        let total=localStorage.getItem('totalProduct');       //setting new total product after delete
-        total--;
-        totalItem.innerText=total;   
-        localStorage.setItem("totalProduct",total);
-        let price=localStorage.getItem("totalMrp");
-        let deletProductPrice=Number(e.target.parentNode.children[0].children[1].innerText);
-        price=price-deletProductPrice;
-        totalAmount.innerText=price;               //totalAmount
-        totalAmountDiscount.innerText=price;        //totalAmountDiscount
-        localStorage.setItem("totalMrp",price);
-        let src=e.target.parentNode.parentNode.parentNode.children[0].children[0].src;
-        let a=localStorage.getItem("productArray");
-        let productObj=(JSON.parse(a));
-        productObj.forEach((storeProduct,i)=>{
-            if(src==storeProduct.itemLink){
-                console.log(storeProduct);
-                productObj.splice(i,1);
-            }
+mainBag.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove-btn")) {
+    e.target.parentNode.parentNode.parentNode.remove();
+    let total = localStorage.getItem("totalProduct"); //setting new total product after delete
+    total--;
+    totalItem.innerText = total;
+    localStorage.setItem("totalProduct", total);    //set totalItem in localstorage after removing item
+    let src =
+      e.target.parentNode.parentNode.parentNode.children[0].children[0].src;
+    let a = localStorage.getItem("productArray") || "[]";
+    let productObj = JSON.parse(a);
+    productObj.forEach((storeProduct, i) => {
+      if (src == storeProduct.itemLink) {
+        console.log(storeProduct);
+        productObj.splice(i, 1);
+      }
+    });
     localStorage.setItem("productArray", JSON.stringify(productObj));
-        })
-        }
-       
-        
-         
+    let price = 0;
+    productObj.forEach((product) => {
+      let p = Number(product.price);
+      price += p;
+      console.log(p);
+    });
+    totalAmount.innerText =  price;
+    totalAmountDiscount.innerText = price; //totalAmountDiscount
+  }
 });
-
-
