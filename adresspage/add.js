df44197e3c1d5ae7ff05ad882a57158119ba0eba
alishabab62 <alishabab62@gmail.com>
2022-12-28 14:18:27 +0530

    code format

--- adresspage/add.js
+++ adresspage/add.js
@@ -1,74 +1,65 @@
-const logo = document.querySelector(".logo")
+const logo = document.querySelector(".logo");
 
-logo.addEventListener('click',(()=>{
-    console.log("vineet")
-    location = '../index.html'
-}));
+logo.addEventListener("click", () => {
+  console.log("vineet");
+  location = "../index.html";
+});
 
-const totalMrp=document.querySelector("#side");
-const totalAmount=document.querySelector('.side-total');
-const discount=document.querySelector("#colorc");
-let totalBill=localStorage.getItem("totalMrp");
-const totalProduct=document.querySelector("#total");
-let discountPrice=Math.floor((totalBill*40)/100);
-discount.innerText=discountPrice;
-totalMrp.innerText=totalBill;
-totalAmount.innerText=totalBill-discountPrice;
-let total=localStorage.getItem("totalProduct");
-totalProduct.innerText=total;
+const totalMrp = document.querySelector("#side");
+const totalAmount = document.querySelector(".side-total");
+const discount = document.querySelector("#colorc");
+let totalBill = localStorage.getItem("totalMrp");
+const totalProduct = document.querySelector("#total");
+let discountPrice = Math.floor((totalBill * 40) / 100);
+discount.innerText = discountPrice;
+totalMrp.innerText = totalBill;
+totalAmount.innerText = totalBill - discountPrice;
+let total = localStorage.getItem("totalProduct");
+totalProduct.innerText = total;
 //addresscode
 const totalDetails = document.querySelector(".details");
-const button = document.querySelector("#b3")
+const button = document.querySelector("#b3");
 
-
-
-
-
-
-
-
-
-
-const userAddres=document.querySelector(".user-address-container");
-window.addEventListener('load',()=>{
-    if(localStorage.getItem("addressDetails")==null){
-        userAddres.style.display="none";
-        totalDetails.style.display="block";
-    }else{
-        button.addEventListener( "click", ((e)=>{
-            console.log(e.target.parentNode.parentNode.childNodes);
-            const addressDetails = e.target.parentNode.parentNode.childNodes
-            let name=addressDetails[3].value;
-            let mobileNo=addressDetails[5].value;
-            let pincode = addressDetails[9].value;
-            let address = addressDetails[11].value;
-            let locality = addressDetails[13].value;
-            let city = addressDetails[15].value;
-            let state = addressDetails[17].value;
-            let allDetailsArray = [{
-                "name": name,
-                "mobileNo" : mobileNo,
-                "pincode":pincode,
-                "address":address,
-                "locality":locality,
-                "city":city,
-                "state":state
-            }]
-            if(localStorage.getItem("addressDetails")==null){
-                console.log("Hello");
-                
-            }else{
-                localStorage.setItem("addressDetails",JSON.stringify(allDetailsArray));
-                userAddres.innerHTML=`<input type="radio"> <name>${name}</name>
+const userAddres = document.querySelector(".user-address-container");
+window.addEventListener("load", () => {
+  if (localStorage.getItem("addressDetails") == null) {
+    userAddres.style.display = "none";
+    totalDetails.style.display = "block";
+  } else {
+    button.addEventListener("click", (e) => {
+      console.log(e.target.parentNode.parentNode.childNodes);
+      const addressDetails = e.target.parentNode.parentNode.childNodes;
+      let name = addressDetails[3].value;
+      let mobileNo = addressDetails[5].value;
+      let pincode = addressDetails[9].value;
+      let address = addressDetails[11].value;
+      let locality = addressDetails[13].value;
+      let city = addressDetails[15].value;
+      let state = addressDetails[17].value;
+      let allDetailsArray = [
+        {
+          name: name,
+          mobileNo: mobileNo,
+          pincode: pincode,
+          address: address,
+          locality: locality,
+          city: city,
+          state: state,
+        },
+      ];
+      if (localStorage.getItem("addressDetails") == null) {
+        console.log("Hello");
+      } else {
+        localStorage.setItem("addressDetails", JSON.stringify(allDetailsArray));
+        userAddres.innerHTML = `<input type="radio"> <name>${name}</name>
                 <div class="address"> <p>${locality} </p><p>${address}</p><p>${city},</p><p>${state}-</p><p>${pincode}</p></div>
                 <p>Mobile <span>${mobileNo}</span></p>
                 <p>Pay on delivery</p>
                 <button>Remove</button>
                 <button>Edit</button>`;
-                // userAddres.style.display="block";
-                // totalDetails.style.display="none";
-            }
-        }));
-    }
+        // userAddres.style.display="block";
+        // totalDetails.style.display="none";
+      }
+    });
+  }
 });
-
--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 // ===========================vineet header js code start===========================
 
 // ==============================For Laptop view===========================
@@ -12,7 +9,7 @@ const header_home = document.querySelector(".header_home");
 const header_beauty = document.querySelector(".header_beauty");
 const header_studio = document.querySelector(".header_studio");
 const header_profile = document.querySelector(".header_icon_div");
-const logo = document.querySelector(".logo")
+const logo = document.querySelector(".logo");
 
 const men = document.querySelector(".men");
 const women = document.querySelector(".women");
@@ -23,100 +20,99 @@ const studio = document.querySelector(".studio");
 const profile = document.querySelector(".profile");
 const profileMouseOut = document.querySelector(".header_profile_submenu_div");
 
-function openSubmenuMen(){
-    // console.log("vineet");
-    men.style.display= "flex";
+function openSubmenuMen() {
+  // console.log("vineet");
+  men.style.display = "flex";
 }
-function closeSubmenuMen(){
-    men.style.display="none";
+function closeSubmenuMen() {
+  men.style.display = "none";
 }
 function openSubmenuWomen() {
-    console.log(women)
-    men.style.display= "flex";
+  console.log(women);
+  men.style.display = "flex";
 }
 function closeSubmenuWomen() {
-    console.log(women)
-    men.style.display= "none";
+  console.log(women);
+  men.style.display = "none";
 }
-function openSubmenuKids(){
-    men.style.display= "flex";
+function openSubmenuKids() {
+  men.style.display = "flex";
 }
-function closeSubmenuKids(){
-    men.style.display= "none";
+function closeSubmenuKids() {
+  men.style.display = "none";
 }
-function openSubmenuHome(){
-    home.style.display= "flex";
+function openSubmenuHome() {
+  home.style.display = "flex";
 }
-function closeSubmenuHome(){
-    home.style.display= "none";
+function closeSubmenuHome() {
+  home.style.display = "none";
 }
-function openSubmenuBeauty(){
-    beauty.style.display= "flex";
+function openSubmenuBeauty() {
+  beauty.style.display = "flex";
 }
-function closeSubmenuBeauty(){
-    beauty.style.display= "none";
+function closeSubmenuBeauty() {
+  beauty.style.display = "none";
 }
-function openSubmenuStudio(){
-    studio.style.display= "flex";
+function openSubmenuStudio() {
+  studio.style.display = "flex";
 }
-function closeSubmenuStudio(){
-    studio.style.display= "none";
+function closeSubmenuStudio() {
+  studio.style.display = "none";
 }
-function openSubmenuProfile(){
-    profile.style.display= "block";
+function openSubmenuProfile() {
+  profile.style.display = "block";
 }
-function closeSubmenuProfile(){
-    profile.style.display= "none";
+function closeSubmenuProfile() {
+  profile.style.display = "none";
 }
-header_men.addEventListener('mouseover', openSubmenuMen);
-header_men.addEventListener('mouseout', closeSubmenuMen);
-header_women.addEventListener('mouseover', openSubmenuWomen);
-header_women.addEventListener('mouseout', closeSubmenuWomen);
-header_kids.addEventListener('mouseover', openSubmenuKids);
-header_kids.addEventListener('mouseout', closeSubmenuKids);
-header_home.addEventListener('mouseover', openSubmenuHome);
-header_home.addEventListener('mouseout', closeSubmenuHome);
-header_beauty.addEventListener('mouseover', openSubmenuBeauty);
-header_beauty.addEventListener('mouseout', closeSubmenuBeauty);
-header_studio.addEventListener('mouseover', openSubmenuStudio);
-header_studio.addEventListener('mouseout', closeSubmenuStudio);
-header_profile.addEventListener('mouseover', openSubmenuProfile);
-profileMouseOut.addEventListener('mouseout', closeSubmenuProfile);
-logo.addEventListener('click',(()=>{
-    location = 'index.html'
-}))
+header_men.addEventListener("mouseover", openSubmenuMen);
+header_men.addEventListener("mouseout", closeSubmenuMen);
+header_women.addEventListener("mouseover", openSubmenuWomen);
+header_women.addEventListener("mouseout", closeSubmenuWomen);
+header_kids.addEventListener("mouseover", openSubmenuKids);
+header_kids.addEventListener("mouseout", closeSubmenuKids);
+header_home.addEventListener("mouseover", openSubmenuHome);
+header_home.addEventListener("mouseout", closeSubmenuHome);
+header_beauty.addEventListener("mouseover", openSubmenuBeauty);
+header_beauty.addEventListener("mouseout", closeSubmenuBeauty);
+header_studio.addEventListener("mouseover", openSubmenuStudio);
+header_studio.addEventListener("mouseout", closeSubmenuStudio);
+header_profile.addEventListener("mouseover", openSubmenuProfile);
+profileMouseOut.addEventListener("mouseout", closeSubmenuProfile);
+logo.addEventListener("click", () => {
+  location = "index.html";
+});
 
 // ===========================For Tab and Mobile view=================
 
 const burgerIcon = document.querySelector("#burgerIcon");
-const sidebar = document.querySelector(".sidebarMenu")
+const sidebar = document.querySelector(".sidebarMenu");
 
-function openSideBar(){
-    // console.log("vineet");
-    // sidebar.style.display="block"
-    if(sidebar.style.display !=="block"){
-        sidebar.style.display = "block"
-    }else{
-        sidebar.style.display = "none";
-    }
+function openSideBar() {
+  // console.log("vineet");
+  // sidebar.style.display="block"
+  if (sidebar.style.display !== "block") {
+    sidebar.style.display = "block";
+  } else {
+    sidebar.style.display = "none";
+  }
 }
 
-
-burgerIcon.addEventListener('click', openSideBar);
+burgerIcon.addEventListener("click", openSideBar);
 
 // ===================== Sale End Timer=================================
 
 const startingTime = 60;
-let time = startingTime *60;
+let time = startingTime * 60;
 
-const timer = document.querySelector('.timer');
+const timer = document.querySelector(".timer");
 
 setInterval(timeupdate, 1000);
 
 function timeupdate() {
-    const min = Math.floor(time/60);
-    let sec = time % 60;
-    timer.innerHTML = `${'00'}:${min}: ${sec}`;
-    time--;
+  const min = Math.floor(time / 60);
+  let sec = time % 60;
+  timer.innerHTML = `${"00"}:${min}: ${sec}`;
+  time--;
 }
 // =================================vineet's js code end======================
